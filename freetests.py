a20c2cfcf3a594d5a4c7e34f17fcda8770d02956
Yuntian Zhang <yuntian1@cs.ualberta.ca>
2019-01-19 17:24:31 -0700

    css misssing

--- freetests.py
+++ freetests.py
@@ -28,6 +28,7 @@ class TestYourWebserver(unittest.TestCase):
     def test_css(self):
         url = self.baseurl + "/base.css"
         req = request.urlopen(url, None, 3)
+
         self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
         self.assertTrue( req.info().get_content_type() == "text/css", ("Bad mimetype for css! %s" % req.info().get_content_type()))
 
