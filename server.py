d8383d649b0756274a77520b0f30c2615df5e43e
zytwh2012 <zytwh2012@gmail.com>
2019-01-23 19:12:38 -0700

    copy right

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #  coding: utf-8 
 import socketserver
 from datetime import datetime
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Yuntian Zhang
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
